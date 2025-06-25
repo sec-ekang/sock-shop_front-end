d54f034fe82dae961cd89a96ada5242194e9058d
sec-ekang <64343357+sec-ekang@users.noreply.github.com>
2025-06-26 01:13:26 +1200

    Update server.js

--- server.js
+++ server.js
@@ -56,3 +56,5 @@ var server = app.listen(process.env.PORT || 8079, function () {
   var port = server.address().port;
   console.log("App now running in %s mode on port %d", app.get("env"), port);
 });
+
+module.exports = app; // Export the app for testing
